b68d272e7ec507ef4c06722f36b3e0f3844ef98a
KoeusIss <sebri.issam@gmail.com>
2020-08-17 22:32:00 +0100

    commit the task static deply

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,7 +13,7 @@ def do_pack():
     """Archive the content of web_static folder"""
     time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
     file_path = "versions/web_static_{}.tgz".format(time)
-    cmd = "tar -cvzf {} web_static".format(file_path)
+    cmd = "tar -cvzf {} web_static/*".format(file_path)
     local("mkdir -p versions")
     local(cmd)
     if os.path.exists(file_path):
@@ -29,9 +29,14 @@ def do_deploy(archive_path):
         file_name = archive_path.split("/")[-1].split(".")[0]
 
         put(archive_path, '/tmp/')
+        # ! just for testing
+        # //run('rm -rf /data/web_static/releases/*')
         run('mkdir /data/web_static/releases/{}'.format(file_name))
-        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'.
+        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}'.
             format(file_name, file_name))
+        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.
+            format(file_name, file_name))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(file_name))
         run('rm -rf /tmp/{}.tgz'.format(file_name))
         run('rm -rf /data/web_static/current')
         run('ln -sf /data/web_static/releases/{}/ /data/web_static/current'.

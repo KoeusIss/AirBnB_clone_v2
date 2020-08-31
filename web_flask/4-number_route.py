0912fe8f13ef1e0ff710baf29a94de3bde6804a2
KoeusIss <sebri.issam@gmail.com>
2020-08-31 14:17:26 +0100

    fix the python documentation

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""WebFlask module"""
 from flask import Flask
 from os import environ
 app = Flask(__name__)
@@ -6,26 +7,36 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_route():
+    """Display Hello HBNB"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb_route():
+    """Display HBNB"""
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
+    """Dipaly text"""
     real_text = text.replace('_', ' ')
     return 'C {}'.format(real_text)
+
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_route(text='is cool'):
+    """Dipaly text"""
     real_text = text.replace('_', ' ')
     return 'Python {}'.format(real_text)
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_route(n):
+    """Dipaly number if integer"""
     return '{}'.format(n)
+
 
 if __name__ == '__main__':
     environ['FLASK_APP'] = __file__

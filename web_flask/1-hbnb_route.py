9d45e878a6190407aee203c046c8da301babfa09
KoeusIss <sebri.issam@gmail.com>
2020-08-31 14:13:30 +0100

    fix the python documentation

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""WebFlask module"""
 from flask import Flask
 from os import environ
 app = Flask(__name__)
@@ -6,11 +7,15 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_route():
+    """Display Hello HBNB"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb_route():
+    """Display HBNB"""
     return 'HBNB'
+
 
 if __name__ == '__main__':
     environ['FLASK_APP'] = __file__
